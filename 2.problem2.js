9852572d8cf8f854b4e3ca3246f506189d7eed48
asm-saim <saim777m@gmail.com>
2026-08-06 23:18:29 +0600

    probelm 2 regarding spread operatoe nad set method

--- 2.problem2.js
+++ 2.problem2.js
@@ -11,10 +11,23 @@
 
 function totalCalc(discount, ...restOp) {
   let total = restOp.reduce((accumulator, element) => accumulator + element, 0);
-    // console.log(total);
+  // console.log(total);
   let discountAmount = (total * discount) / 100;
-//   console.log(discountAmount);
+  //   console.log(discountAmount);
   let discountedPrice = total - discountAmount;
   return discountedPrice;
 }
 console.log(totalCalc(10, 24, 56, 345, 234, 112));
+
+/*
+  Problem 2: Merge & Deduplicate Arrays
+  Merge two arrays and remove duplicates using spread + Set.
+
+  Example: [1,2,3] + [2,3,4] -> [1,2,3,4]
+*/
+const num1 = [1, 2, 3];
+const num2 = [2, 3, 4];
+
+let newMarge = [...num1, ...num2];
+let unique = Array.from(new Set(newMarge));
+console.log(unique);
