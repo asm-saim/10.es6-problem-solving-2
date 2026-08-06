86019f6af78de1e2cc1a5d1ca89a0401d012e52a
asm-saim <saim777m@gmail.com>
2026-08-06 16:08:25 +0600

    template stirng implemented

--- 1.problem1.js
+++ 1.problem1.js
@@ -7,3 +7,16 @@ for (let i = 1; i <= 3; i++) {
 }
 
 console.log("Final Print: ", sum);
+
+/*
+  Problem 2: Receipt Generator (Template Strings)
+  Build a receipt generator using template strings — take item name,
+  price, qty as input, output formatted multiline receipt.
+
+  Example: Input ("Pen", 20, 3) -> Output "Pen x3 = 60 Taka"
+*/
+function receiptGenerator(product, price, qty) {
+  return `${product} x${qty} = ${price * qty} Taka`;
+}
+console.log(receiptGenerator("Pen", 20, 3));
+console.log(receiptGenerator("Pencil", 8, 4));
