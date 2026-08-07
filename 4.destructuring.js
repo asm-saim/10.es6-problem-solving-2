e9dc9d479bd1b3f12a9afc9709b15cbfad6e87e3
asm-saim <saim777m@gmail.com>
2026-08-07 10:22:00 +0600

    destructuring implemented

---
+++ 4.destructuring.js
@@ -0,0 +1,16 @@
+/*
+  Problem 1: Nested Response Extractor
+  Extract specific fields from a nested API-like response object using
+  destructuring, with renaming + default value.
+
+  Example: { user: { name: "Rafi", age: 22 } } -> extract name as
+  userName, default age = 18 if missing
+*/
+
+const extractor = (obj) => {
+  const {
+    user: { name: userName, age = 18 },
+  } = obj;
+  return {userName, age};
+};
+console.log(extractor({ user: { name: "Rafi" } }));
