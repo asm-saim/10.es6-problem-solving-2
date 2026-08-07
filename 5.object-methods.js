ad64af6b18cbb59d31dce3ad0473ad748d08b8c9
asm-saim <saim777m@gmail.com>
2026-08-07 16:28:04 +0600

    customized optional chaining

--- 5.object-methods.js
+++ 5.object-methods.js
@@ -19,3 +19,25 @@ const expensive = (obj) => {
   return expProduct;
 };
 console.log(expensive({ pen: 20, book: 150, bag: 500 }));
+
+/*
+  Problem 2: Safe Nested Access
+  Safely access deeply nested optional data using ?. and ?? without
+  throwing errors.
+
+  Example: user?.address?.city ?? "City not found" when address is undefined
+*/
+
+const info = {
+  name: "KM",
+  country: "USA",
+  location: { city: "NYC", street: { road: "123/BC" } },
+};
+const info2 = {
+  name: "KM",
+  country: "USA",
+  location: { city: "NYC" },
+};
+
+console.log(info.location?.street?.road);
+console.log(info2.location?.street?.road??"Not Found");
