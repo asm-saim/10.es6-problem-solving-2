a5259bb3e1b7aeeac30d5a81d485da77ec4eba7b
asm-saim <saim777m@gmail.com>
2026-08-07 21:33:03 +0600

    practice problem

---
+++ 7.problem-solving.js
@@ -0,0 +1,16 @@
+// Task 1: calculateBill(amount, taxRate = 0.05) — Default Parameters
+// Return the final bill after tax is applied. Default tax rate is 5% when not provided.
+// Input:
+// calculateBill(1000)
+// Output:
+// 1050
+// Input:
+// calculateBill(1000, 0.10)
+// Output:
+// 1100
+
+const calculateBill = (amount, taxRate = 0.05) => {
+  let totalTex = amount * taxRate;
+  return amount + totalTex;
+};
+console.log(calculateBill(1000, 0.10));
